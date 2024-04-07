adf5df6a71741e16f93a581e4866c3f992246040
vorontsov <mimictear@outlook.com>
2024-04-07 18:51:39 +0200

    Initial Commit

---
+++ Tower of Hanoi/Tower_of_HanoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tower_of_HanoiApp.swift
+//  Tower of Hanoi
+//
+//  Created by ANDREY VORONTSOV on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tower_of_HanoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
